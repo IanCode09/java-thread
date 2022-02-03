abebe22283f748c6fb56b6612d2370c83a3d2f46
Ian Lombu <citrakemurnianlombu@gmail.com>
2022-02-03 23:38:00 +0700

    first commit

---
+++ java-thread/src/test/java/com/woyo/javathread/JavaThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woyo.javathread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
